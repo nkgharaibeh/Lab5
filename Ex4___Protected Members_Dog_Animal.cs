1c963ac7fe3b3ba7538c73f96fce8ee23c1074fb
Natheer Khlaif Gharaibeh <nkgharaibeh@gmail.com>
2025-12-26 18:32:20 +0300

    Update Ex4___Protected Members_Dog_Animal.cs

--- Ex4___Protected Members_Dog_Animal.cs
+++ Ex4___Protected Members_Dog_Animal.cs
@@ -3,13 +3,16 @@ using System;
 class Animal
 {
     protected string Type = "Mammal";
+        // 'protected' members are accessible ONLY:
+        // 1) inside the same class (Animal)
+        // 2) inside derived classes (Dog)
 }
 
 class Dog : Animal
 {
     public void ShowType()
     {
-        Console.WriteLine("Animal Type: " + Type);
+        Console.WriteLine("Animal Type: " + Type);  // Accessing protected member (Type) from base class inside derived class:
     }
 }
 
